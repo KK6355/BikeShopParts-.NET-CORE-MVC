c2eff3e7b3be34619b9af28bad8a2f2592f9bd97
KK6355 <kangjiawei666@126.com>
2022-10-14 19:54:20 +1300

    Rendered linq query on index page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,6 +20,23 @@ namespace BikeShopPartsMVC.Controllers
 
         public IActionResult Index()
         {
+            var totalCustomers = _context.Customer.Count();
+            ViewData["CustomerCount"] = totalCustomers;
+            var totalStock = _context.Stock.Count();
+            ViewData["StockCount"] = totalStock;
+            var totalOrders = _context.Order.Count();
+            ViewData["OrderCount"] = totalOrders;
+            var totalStaff = _context.Staff.Count();
+            ViewData["StaffCount"] = totalStaff;
+            var customersMadeOrder = _context.Order.GroupBy(o=>o.CustomerId).Count();
+            ViewData["customersMadeOrder"] = customersMadeOrder;
+            var productTypes = _context.Stock.GroupBy(s => s.ProductType).Count();
+            ViewData["productTypes"] = productTypes;
+
+            var thisYearOrder = _context.Order.Where(o => o.OrderDate.Value.Year == DateTime.Now.Year).Count();
+            ViewData["thisYearOrder"] = thisYearOrder;
+            //var topSales = _context.Order.GroupBy(o => o.Staff.Name).Take(1).ToString();
+            //ViewData["topSales"] = topSales;
             return View();
         }
 
